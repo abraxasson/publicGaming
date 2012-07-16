f48d5edece14b36267bdf371a032acb639b7356f
Manuel Zametter <m.zametter@gmail.com>
2012-07-16 13:43:56 +0200

    Error fixing

--- House of Fire - Desktop/HOF-Desktop/src/hof/core/FullScreen.java
+++ House of Fire - Desktop/HOF-Desktop/src/hof/core/FullScreen.java
@@ -1,4 +1,4 @@
-package houseOfFire;
+package hof.core;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
--- House of Fire - Desktop/HOF-Desktop/src/hof/core/Main.java
+++ House of Fire - Desktop/HOF-Desktop/src/hof/core/Main.java
@@ -1,4 +1,4 @@
-package houseOfFire;
+package hof.core;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
