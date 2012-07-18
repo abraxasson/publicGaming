5cae4c88c915f26e123f37576c14124a2ee2e180
Florian Genser <florian.genser@gmail.com>
2012-07-18 15:27:21 +0200

    FireFighter implementiert
    Der FireFighter kann jetzt gezeichnet werden und sich nach links/rechts
    bewegen.

--- House of Fire - Desktop/HOF-Game/src/hof/core/PlayingScreen.java
+++ House of Fire - Desktop/HOF-Game/src/hof/core/PlayingScreen.java
@@ -2,23 +2,68 @@ package hof.core;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import hof.core.utils.Assets;
 import hof.core.utils.GameScreen;
+import hof.level.objects.Firefighter;
+import hof.player.Player;
 
 public class PlayingScreen extends GameScreen<HouseOfFireGame> {
 
+	Firefighter firefighter;
+	BitmapFont font;
+	
+	String txt = "";
+	
 	public PlayingScreen(HouseOfFireGame game) {
 		super(game);
+		spriteBatch = new SpriteBatch();
+		firefighter = new Firefighter(Assets.pureWhiteTextureRegion.getTexture(), Gdx.graphics.getWidth() / 2, 
+				0, 40, 80, new Player("Florian", null, Color.PINK));
+
+		font = Assets.textFont;
 	}
 	
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0.3f, 0.3f, 1f, 1);
+		Gdx.gl.glClearColor(0f, 0f, 0f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		spriteBatch.begin();
+		firefighter.draw(spriteBatch);
+		font.draw(spriteBatch, txt, 600, 1000);
+		spriteBatch.end();
+		
+		if (Gdx.input.isKeyPressed(Keys.RIGHT) || Gdx.input.isKeyPressed(Keys.D)) {
+			
+			int d = firefighter.getX();
+			int x =  d + (int) (300 * Gdx.graphics.getDeltaTime());
+			firefighter.setX(x);
+		}
+		
+		if (Gdx.input.isKeyPressed(Keys.LEFT) || Gdx.input.isKeyPressed(Keys.A)) {
+			
+			int d = firefighter.getX();
+			int x =  d - (int) (300 * Gdx.graphics.getDeltaTime());
+			firefighter.setX(x);
+		}
+		
+		keepInBounds();
 		
 		if (Gdx.input.isKeyPressed(Keys.BACKSPACE)) {
 			game.setScreen(game.mainMenuScreen);
 		}
+		
+		if (Gdx.input.isKeyPressed(Keys.ESCAPE)) {
+			System.exit(0);
+		}
+	}
+	
+	private void keepInBounds() {
+		firefighter.stayInBounds();
 	}
 }
--- House of Fire - Desktop/HOF-Game/src/hof/level/objects/Firefighter.java
+++ House of Fire - Desktop/HOF-Game/src/hof/level/objects/Firefighter.java
@@ -1,7 +1,11 @@
 package hof.level.objects;
 
 import hof.player.Player;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Firefighter extends AbstractPerson {
 	private Player player;
@@ -13,5 +17,19 @@ public class Firefighter extends AbstractPerson {
 
 	public Player getPlayer() {
 		return player;
+	}
+	
+	public void draw(SpriteBatch spriteBatch) {
+		Color oldColor = spriteBatch.getColor();
+		spriteBatch.setColor(getPlayer().getColor());
+		spriteBatch.draw(getBody(), getX(), getY(), getWidth(), getHeight(), 0, 0, 8, 8, false, false);
+		spriteBatch.setColor(oldColor);
+	}
+	
+	public void stayInBounds() {
+		if (getX() < 0) 
+			setX(0);
+		if (getX() > Gdx.graphics.getWidth() - getWidth())
+			setX(Gdx.graphics.getWidth() - getWidth());
 	}
 }
