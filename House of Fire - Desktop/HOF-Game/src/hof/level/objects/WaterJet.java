a2bcd057415f0ebbea2ade07c69e6cd00079eb5e
Florian Genser <florian.genser@gmail.com>
2012-07-18 08:57:16 +0200

    WaterJet Klasse erzeugt
    Diese Klasse dient zur Darstellung des Wasserstrahls und speichert alle f�r diesen relevante Daten.

---
+++ House of Fire - Desktop/HOF-Game/src/hof/level/objects/WaterJet.java
@@ -0,0 +1,20 @@
+package hof.level.objects;
+
+import hof.core.utils.Assets;
+
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
+
+public class WaterJet {
+
+	private Firefighter player;
+	private ParticleEffect waterJet;
+	
+	public WaterJet(Firefighter player) {
+		this.player = player;
+		waterJet = Assets.waterParticleEffect;
+	}
+	
+	public void update () {
+		
+	}
+}
