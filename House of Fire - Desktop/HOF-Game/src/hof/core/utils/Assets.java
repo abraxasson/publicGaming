d13117ea231252140b8f785b2178d4e784926558
Florian Genser <florian.genser@gmail.com>
2012-07-18 08:55:54 +0200

    Particelfolder in Assets eingefügt
    Eigenen Subfolder particles erzeugt.
    Auch in der zentralen Ladeklasse Assets wurden Particles zum Laden
    eingefügt.
    Ein Wasserstrahl und 6 Feuer werden im Moment geladen.
    Die Anzahl der einzelnen Feuer wird durch die Konstante FIRES
    festgelegt.

--- House of Fire - Desktop/HOF-Game/src/hof/core/utils/Assets.java
+++ House of Fire - Desktop/HOF-Game/src/hof/core/utils/Assets.java
@@ -3,6 +3,7 @@ package hof.core.utils;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.ParticleEffect;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
@@ -14,6 +15,10 @@ public class Assets {
 	
 	public static Sound buttonSound;
 	
+	public static ParticleEffect waterParticleEffect;
+	public static ParticleEffect[] fireParticleEffect;
+	
+	private static final int FIRES = 6;
 	
 	public static BitmapFont defaultFont;
 	
@@ -25,8 +30,21 @@ public class Assets {
 		createAnimations();
 		loadSounds();
 		loadFonts();
+		loadParticles();
 	}
 	
+	private static void loadParticles() {
+		waterParticleEffect = new ParticleEffect();
+		waterParticleEffect.load(Gdx.files.internal("particles/waterjet.p"), Gdx.files.internal("particles"));
+		
+		fireParticleEffect = new ParticleEffect[FIRES];
+		for (int i = 0; i < fireParticleEffect.length; i++) {
+			fireParticleEffect[i] = new ParticleEffect();
+			fireParticleEffect[i].load(Gdx.files.internal("particles/fire.p"), Gdx.files.internal("particles"));
+		}
+		
+	}
+
 	private static void createAnimations() {
 		
 	}
