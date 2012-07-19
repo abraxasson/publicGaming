976d17c2fd808fd1509d7d10cc75af5f8bbc8b8f
Florian Genser <florian.genser@gmail.com>
2012-07-19 16:39:31 +0200

    GameOverScreen aktualisiert
    Bei GameOver wird ein Text angezeigt und das Programm endet nach 4
    Sekunden.

--- House of Fire - Desktop/HOF-Game/src/hof/core/GameOverScreen.java
+++ House of Fire - Desktop/HOF-Game/src/hof/core/GameOverScreen.java
@@ -1,23 +1,42 @@
 package hof.core;
 
+
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 
+import hof.core.utils.Assets;
 import hof.core.utils.GameScreen;
 
 public class GameOverScreen extends GameScreen<HouseOfFireGame> {
 
+	private long startTime;
+	
 	public GameOverScreen(HouseOfFireGame game) {
 		super(game);
+		
+	}
+	
+	@Override
+	public void show() {
+		startTime = System.currentTimeMillis();
 	}
 	
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		if (Gdx.input.isKeyPressed(Keys.ESCAPE)) {
+			System.exit(0);
+		}
+		
+		spriteBatch.begin();
+		Assets.textFont.draw(spriteBatch, "GAME - OVER", Assets.CANVAS_WIDTH / 2, Assets.CANVAS_HEIGHT / 2);
+		spriteBatch.end();
+		
+		if (System.currentTimeMillis() - startTime >= 4000l) {
 			System.exit(0);
 		}
 	}
