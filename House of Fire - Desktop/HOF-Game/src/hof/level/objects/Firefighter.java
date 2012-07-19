7ab4665d766bdd3d044aa685a7f20988c0733b98
Florian Genser <florian.genser@gmail.com>
2012-07-19 09:27:12 +0200

    WaterJet implementiert
    Befindet sich automatisch auf der Position des zugehörigen FireFighters
    und die Stärke kann durch drücken von Up/W and Down/S verändert werden.
    Desweiteren kann der Winkel durch A und D beeinflusst werden.

--- House of Fire - Desktop/HOF-Game/src/hof/level/objects/Firefighter.java
+++ House of Fire - Desktop/HOF-Game/src/hof/level/objects/Firefighter.java
@@ -9,10 +9,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Firefighter extends AbstractPerson {
 	private Player player;
+	private WaterJet waterJet;
 	
 	public Firefighter(Texture body, int x, int y, int width, int height, Player player) {
 		super(body, x, y, width, height);
 		this.player = player;
+		waterJet = new WaterJet();
 	}
 
 	public Player getPlayer() {
@@ -20,16 +22,26 @@ public class Firefighter extends AbstractPerson {
 	}
 	
 	public void draw(SpriteBatch spriteBatch) {
+		updateJet();
+		waterJet.draw(spriteBatch);
 		Color oldColor = spriteBatch.getColor();
 		spriteBatch.setColor(getPlayer().getColor());
 		spriteBatch.draw(getBody(), getX(), getY(), getWidth(), getHeight(), 0, 0, 8, 8, false, false);
 		spriteBatch.setColor(oldColor);
 	}
 	
+	private void updateJet() {
+		waterJet.setPosition(getX(), getY());
+	}
+
 	public void stayInBounds() {
 		if (getX() < 0) 
 			setX(0);
 		if (getX() > Gdx.graphics.getWidth() - getWidth())
 			setX(Gdx.graphics.getWidth() - getWidth());
+	}
+
+	public WaterJet getWaterJet() {
+		return waterJet;		
 	}
 }
