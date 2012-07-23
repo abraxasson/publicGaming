1eac2f4c2656ace10784e485ce484dc97c110410
Florian Genser <florian.genser@gmail.com>
2012-07-23 15:04:27 +0200

    Zu WaterJet ein Rectangle hinzugefügt
    Diese Rechteck soll zeigen wohin der Strahl zeigt. Dies dient der
    Collision Detection.
    Jedoch ist die momentane Lösung noch recht fehlerhaft.

--- House of Fire - Desktop/HOF-Game/src/hof/level/objects/WaterJet.java
+++ House of Fire - Desktop/HOF-Game/src/hof/level/objects/WaterJet.java
@@ -3,18 +3,32 @@ package hof.level.objects;
 import hof.core.utils.Assets;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
 import com.badlogic.gdx.graphics.g2d.ParticleEmitter;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class WaterJet {
 
 	private ParticleEffect waterJet;
 	private ParticleEmitter emmitter;
+	private Rectangle streamArea;
+	private float size;
 	
 	public WaterJet() {
 		waterJet = Assets.loadWaterParticles();
 		emmitter = waterJet.getEmitters().get(0);
+		
+		emmitter.getLife().getHighMin();
+		emmitter.getAngle().getLowMin();
+		
+		size = 100;
+		streamArea = new Rectangle();
+		streamArea.setX(calcX());
+		streamArea.setY(calcY());
+		streamArea.setWidth(size);
+		streamArea.setHeight(size);
 	}
 	
 	public void setPosition(float x, float y) {
@@ -25,7 +39,7 @@ public class WaterJet {
 		float strength = emmitter.getLife().getHighMax();
 		strength += change * Gdx.graphics.getDeltaTime();
 		emmitter.getLife().setHigh(strength);
-		emmitter.getLife().setHighMin(strength-change);
+		emmitter.getLife().setHighMin(strength - change);
 	}
 	
 	public void setAngle(int val ) {
@@ -35,7 +49,46 @@ public class WaterJet {
 		emmitter.getAngle().setHigh(angle - 10, angle + 10);
 	}
 	
+	public void changeDiameter(float diameter) {
+		size = diameter;
+		updateRectangle();
+	}
+	
+	private int calcX() {
+		int x = 0;
+		float length = emmitter.getLife().getHighMin();
+		float angle = emmitter.getAngle().getLowMin();
+		x =(int) (Math.cos(angle) * length);
+		return x;
+	}
+	
+	private int calcY() {
+		int y = 0;
+		float length = emmitter.getLife().getHighMin();
+		float angle = emmitter.getAngle().getLowMin();
+		y =(int) (Math.sin(angle) * length);
+		return y;
+	}
+	
+	private void updateRectangle() {
+		streamArea.setX(calcX());
+		streamArea.setY(calcY());
+		streamArea.setWidth(size);
+		streamArea.setHeight(size);
+		System.out.println(streamArea);
+	}
+	
+	public Rectangle getStreamArea() {
+		return streamArea;
+	}
+	
 	public void draw (SpriteBatch spriteBatch) {
+		updateRectangle();
 		waterJet.draw(spriteBatch, Gdx.graphics.getDeltaTime());
+		Color oldColor = spriteBatch.getColor();
+		spriteBatch.setColor(Color.BLACK);
+		
+		spriteBatch.draw(Assets.pureWhiteTextureRegion.getTexture(), streamArea.x, streamArea.y, streamArea.width, streamArea.height, 0, 0, 8, 8, false, false);
+		spriteBatch.setColor(oldColor);
 	}
 }
