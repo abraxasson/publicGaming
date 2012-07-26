c9095da4954ab2aa69a9b32787faa4766a9869d6
Florian Genser <florian.genser@gmail.com>
2012-07-26 13:22:39 +0200

    AchievementInfoMessage entfernt
    Dies wird nun in der LevelInfoMessage behandelt.
    Die LevelInfoMessage enth�lt nun einen �berladenen Konstruktor. Darin kann angegeben werden ob das aktuelle Level das letzte Level ist und ob eine Medaille vorhanden ist. Dies wird durch Konstaten abgefragt.

--- House of Fire - Desktop/HOF-Game/src/hof/net/userMessages/LevelInfoMessage.java
+++ House of Fire - Desktop/HOF-Game/src/hof/net/userMessages/LevelInfoMessage.java
@@ -3,17 +3,41 @@ package hof.net.userMessages;
 public class LevelInfoMessage extends AbstractMessage {
 	public static final int STARTED = 1;
 	public static final int FINISHED = 2;
+	
+	public static final int NOTHING = 4;
+	public static final int BRONZE = 6;
+	public static final int SILVER = 8;
+	public static final int GOLD = 10;
 	private static final long serialVersionUID = 37295872L;
 	private int level;
 	private int eventType;
+	private boolean lastLevel;
+	private int medal;
 	
 	
-	public LevelInfoMessage(int eventType, int level) {
+	public LevelInfoMessage(int eventType, int level, boolean lastLevel) {
 		super(Type.LevelInfo);
 		this.level = level;
 		this.eventType = eventType;
+		this.lastLevel = lastLevel;
+		this.medal = NOTHING;
+	}
+	
+	public LevelInfoMessage(int eventType, int level, boolean lastLevel, int medal) {
+		super(Type.LevelInfo);
+		this.level = level;
+		this.eventType = eventType;
+		this.lastLevel = lastLevel;
+		this.medal = medal;
 	}
 
+	public int getMedal() {
+		return medal;
+	}
+
+	public boolean isLastLevel() {
+		return lastLevel;
+	}
 
 	public int getEventType() {
 		return eventType;
