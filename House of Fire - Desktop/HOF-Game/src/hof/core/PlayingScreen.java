e94403a9593f4df7ffb4f5faa292840541ba3287
Manuel Zametter <m.zametter@gmail.com>
2012-07-26 12:48:50 +0200

    Steuerung angepasst!

--- House of Fire - Desktop/HOF-Game/src/hof/core/PlayingScreen.java
+++ House of Fire - Desktop/HOF-Game/src/hof/core/PlayingScreen.java
@@ -99,11 +99,11 @@ public class PlayingScreen extends GameScreen<HouseOfFireGame> {
 			for (Firefighter fighter : firefighters) {
 				if (fighter.getPlayer().getIp()
 						.equals(input.getPlayer().getIp())) {
-					if (input.getMessage().getX() < -0.01) {
+					if (input.getMessage().getX() > 1) {
 						fighter.getWaterJet().setAngle(-3);
 					} else if (input.getMessage().getX() == 0) {
 						System.out.println("Gleichgewicht!");
-					} else if (input.getMessage().getX() > 0.01) {
+					} else if (input.getMessage().getX() < -1) {
 						fighter.getWaterJet().setAngle(3);
 					}
 					/*
