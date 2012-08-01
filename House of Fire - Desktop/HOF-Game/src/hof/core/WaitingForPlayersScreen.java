bb86c44ff6b1e096d8d5f348d028483916d4645f
Florian Genser <florian.genser@gmail.com>
2012-08-01 17:03:01 +0200

    WaitingForPlayerScreen angepasst
    Der erste Teil der Screens wurde eingebaut.

--- House of Fire - Desktop/HOF-Game/src/hof/core/WaitingForPlayersScreen.java
+++ House of Fire - Desktop/HOF-Game/src/hof/core/WaitingForPlayersScreen.java
@@ -5,7 +5,6 @@ import hof.core.utils.GameScreen;
 import hof.core.utils.HallOfFame;
 import hof.net.MessageProcessing;
 import hof.net.UdpClientThread;
-import hof.net.UdpServerThread;
 import hof.net.userMessages.PlayerInfoMessage;
 
 import java.net.InetAddress;
@@ -13,37 +12,21 @@ import java.net.UnknownHostException;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 public class WaitingForPlayersScreen extends GameScreen<HouseOfFireGame> {
 
-	private BitmapFont font;
-	private SimpleButton returnButton;
-	
-	@SuppressWarnings("unused")
-	private UdpServerThread udpServer;
 	private MessageProcessing processing;
 	
 	private HallOfFame fame;
 	
 	private boolean isWaiting;
-    private boolean wasTouched;
     
     private float stateTime;
 	
 	public WaitingForPlayersScreen(HouseOfFireGame game) {
 		super(game);
-		udpServer = UdpServerThread.getInstance();
 		processing = MessageProcessing.getInstance();
-		
-		font = Assets.text50Font;
-		font.setColor(0, 0, 0, 1);
-		
-		returnButton = new SimpleButton("Return", Assets.text50Font, Color.BLACK);
-		returnButton.centerHorizontallyOn(Gdx.graphics.getWidth() / 2);
-		returnButton.topOn(Gdx.graphics.getHeight() / 8);
 		
 		fame = HallOfFame.getInstance();
 	}
@@ -59,11 +42,10 @@ public class WaitingForPlayersScreen extends GameScreen<HouseOfFireGame> {
 	@Override
 	public void render(float delta) {
 		if (isWaiting) {
-			updateButtons(delta);
-			Gdx.gl.glClearColor(1, 1, 1, 1);
+			Gdx.gl.glClearColor(0, 0, 0, 1);
 			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 			
-			if (Gdx.input.isKeyPressed(Keys.BACKSPACE) || returnButton.wasPressed()) {
+			if (Gdx.input.isKeyPressed(Keys.BACKSPACE) ) {
 				game.setScreen(game.mainMenuScreen);
 			}
 			
@@ -88,10 +70,8 @@ public class WaitingForPlayersScreen extends GameScreen<HouseOfFireGame> {
 //				Gdx.app.exit();
 			}
 			spriteBatch.begin();
-			fame.draw(spriteBatch, Assets.text50Font, Color.BLACK);
-			String text = "Waiting for Players";
-			font.draw(spriteBatch, text, Gdx.graphics.getWidth()/2 - font.getBounds(text).width /2, Gdx.graphics.getHeight() / 4);
-			returnButton.draw(spriteBatch);
+			spriteBatch.draw(Assets.waitingForPlayerTitle,Assets.FRAME_WIDTH / 2 - Assets.waitingForPlayerTitle.getWidth()/2 ,Assets.FRAME_HEIGHT /2 - Assets.waitingForPlayerTitle.getHeight()/2);
+//			fame.draw(spriteBatch, Assets.text50Font, Color.BLACK);
 			spriteBatch.end();
 			
 			if (!processing.getPlayerList().isEmpty()) {
@@ -101,15 +81,5 @@ public class WaitingForPlayersScreen extends GameScreen<HouseOfFireGame> {
 			game.setScreen(game.playingScreen);
 		}	
 		
-	}
-	
-	private void updateButtons(float delta) {
-		touchPoint = screenToViewport(Gdx.input.getX(), Gdx.input.getY());
-        boolean justTouched = Gdx.input.justTouched();
-        boolean isTouched = Gdx.input.isTouched();
-        boolean justReleased = wasTouched && !isTouched;
-        wasTouched = isTouched;
-
-		returnButton.update(delta, justTouched, isTouched, justReleased, touchPoint.x, touchPoint.y);
 	}
 }
