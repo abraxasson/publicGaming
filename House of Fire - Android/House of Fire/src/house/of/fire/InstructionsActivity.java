b4384a20d1b9ee8a5ed2484271e18d8ea5d0ce82
Florian Genser <florian.genser@gmail.com>
2013-04-09 18:50:27 +0200

    bin und gen entfernt
    Erzeugte Daten aus dem Repository entfernt.

--- House of Fire - Android/House of Fire/src/house/of/fire/InstructionsActivity.java
+++ House of Fire - Android/House of Fire/src/house/of/fire/InstructionsActivity.java
@@ -1,12 +1,7 @@
 package house.of.fire;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.view.View.OnClickListener;
+import android.os.Bundle;
 
 public class InstructionsActivity extends Activity {
 	
